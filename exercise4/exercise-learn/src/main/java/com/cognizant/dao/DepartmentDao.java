f5594bc7e6e562c7e99127cb9ad53b4887e6f1ca
SruthiHacker <sruthi092004@gmail.com>
2025-07-12 21:17:29 +0530

    Delete exercise4 directory

--- exercise4/exercise-learn/src/main/java/com/cognizant/dao/DepartmentDao.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.dao;
-
-import com.cognizant.model.Department;
-import java.util.List;
-
-public interface DepartmentDao {
-    List<Department> getAllDepartments();
-}
--- exercise4/exercise-learn/src/main/java/com/cognizant/service/DepartmentService.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.service;
-
-import com.cognizant.model.Department;
-import java.util.List;
-
-public interface DepartmentService {
-    List<Department> getAllDepartments();
-}
