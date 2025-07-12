153a53ef27d170bc24fee978d19bf1fa06e97853
SruthiHacker <sruthi092004@gmail.com>
2025-07-12 21:15:30 +0530

    Delete exercise1 directory

--- exercise1/employee-crud-app/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-
-export const routes: Routes = [
-  { path: '', component: EmployeeListComponent },
-  { path: 'edit/:id', component: EditEmployeeComponent },
-];
